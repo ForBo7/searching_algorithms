ab7b4396111f8cf76ff8f2782f78fe37e93bc714
ForzBox7 <salmananaqvii+github@gmail.com>
2021-07-04 21:18:23 +0500

    Implemented two more unit tests.

--- linear_search/test_linear_search.py
+++ linear_search/test_linear_search.py
@@ -14,6 +14,26 @@ class LinearSearchTestCase(unittest.TestCase):
         index = linear_search(list, element)
         self.assertEqual(4, index)
 
+    def test_search_for_element_in_single_element_list(self):
+        """
+        Tests whether the linear search can find a required element in a list
+        that is of a single element and return its correct index.
+        """
+        list = [7]
+        element = 7
+        index = linear_search(list, element)
+        self.assertEqual(0, index)
+
+    def test_search_for_non_existent_element(self):
+        """
+        Tests whether the linear search correctly returns None in the case of
+        the required element not existing.
+        """
+        list = [3, 8, 2, 5, 9, 1, 4]
+        element = 6
+        index = linear_search(list, element)
+        self.assertEqual(None, index)
+
 
 if __name__ == '__main__':
     unittest.main()
