fbe3f94cccc83f94283fc4117ae9f075f71760b5
ForzBox7 <salmananaqvii+github@gmail.com>
2021-07-06 11:38:40 +0500

    Implemented a test case for the binary search.

---
+++ binary_search/test_binary_search.py
@@ -0,0 +1,40 @@
+import unittest
+from binary_search import binary_search
+
+
+class BinarySearchTestCase(unittest.TestCase):
+    """Tests for binary search."""
+
+    def test_search_for_element(self):
+        """
+        Tests whether the binary search can find a required element and return
+        its correct index.
+        """
+        list = [3, 8, 2, 5, 9, 1, 4]
+        element = 9
+        index = binary_search(list, element)
+        self.assertEqual(4, index)
+
+    def test_search_for_element_in_single_element_list(self):
+        """
+        Tests whether the binary search can find a required element in a list
+        that is of a single element and return its correct index.
+        """
+        list = [7]
+        element = 7
+        index = binary_search(list, element)
+        self.assertEqual(0, index)
+
+    def test_search_for_non_existent_element(self):
+        """
+        Tests whether the binary search correctly returns None in the case of
+        the required element not existing.
+        """
+        list = [3, 8, 2, 5, 9, 1, 4]
+        element = 6
+        index = binary_search(list, element)
+        self.assertEqual(None, index)
+
+
+if __name__ == '__main__':
+    unittest.main()
